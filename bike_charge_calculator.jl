8b99ed47b97307c5291e8f3cdc7b2d0bb70e867e
Oliver Blanthorn <freedom4cows@gmail.com>
2022-04-01 23:30:20 +0200

    Initial commit

---
+++ bike_charge_calculator.jl
@@ -0,0 +1,16 @@
+#!/bin/julia
+
+MAX_VOLTAGE = 42
+MIN_VOLTAGE = 36
+
+MAX_AH = 19
+CHARGER_AMPS = 3
+EFFICIENCY = 0.8
+CHARGE_RATE = MAX_AH/CHARGER_AMPS * EFFICIENCY
+
+DESIRED_CHARGE = 0.8
+
+# li-ion cells have pretty linear discharge profiles so this is a reasonable estimate
+charge_level(voltage) = (voltage-MIN_VOLTAGE)/(MAX_VOLTAGE-MIN_VOLTAGE)
+
+time_to_charge(current_voltage, desired_charge=DESIRED_CHARGE) = CHARGE_RATE * (desired_charge - charge_level(current_voltage))
